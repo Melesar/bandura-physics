c734d442f16e6a2326ae367872fc0bb5dd8e58b1
Serhii Pikhota <serhii.pikhota@proton.me>
2026-06-23 16:29:22 +0200

    Add features testing scenario

--- demos/scenarios/features.c
+++ demos/scenarios/features.c
@@ -1,15 +1,9 @@
 #include "scenario-core.h"
-#include "bnd-math.h"
-
-static bnd_body_handle capsule;
 
 void scenario_configure(program_config *config, bnd_config *physics_config) {
-  config->window_title = "Box-capsule collisions";
+  config->window_title = "Features";
   config->camera_position = (bnd_v3){ 22.542, 11.645, 20.752 };
   config->camera_target = (bnd_v3){ 0, 0, 0 };
-
-  physics_config->simulation.gravity = bnd_v3_zero();
-  physics_config->advanced.resolution_attempts_factor = 0;
 }
 
 void scenario_initialize(bnd_world *world) {
@@ -17,18 +11,15 @@ void scenario_initialize(bnd_world *world) {
 }
 
 void scenario_setup_scene(bnd_world *world) {
-  bnd_add_box_dynamic(world, 5, (bnd_v3) { 3, 1, 5 });
+  bnd_body_handle floor = bnd_add_box_static(world, (bnd_v3) { 10, 1, 10 }).value;
+  bnd_body_handle box = bnd_add_box_dynamic(world, 5, (bnd_v3) { 1.5, 1.5, 1.5 }).value;
 
-  capsule = bnd_add_capsule_dynamic(world, 5, 1, 1.5).value;
-  bnd_set_position(world, capsule, (bnd_v3) { 0, 3, 0 });
-
-  register_gizmo(world, capsule);
+  bnd_set_position(world, box, (bnd_v3) { 0, 7, 0 });
 }
 
 void scenario_handle_input(bnd_world *world, Camera *camera) { }
 
 void scenario_simulate(bnd_world *world, float dt) {
-  bnd_awaken_body(world, capsule);
   bnd_simulate(world, dt);
 }
 
