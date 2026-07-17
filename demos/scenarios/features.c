8ce8449f37c6e6024e6f1e8971637ca0c8a08599
Serhii Pikhota <serhii.pikhota@proton.me>
2026-07-17 17:25:54 +0200

    Replace compound with a mesh in features

--- demos/scenarios/features.c
+++ demos/scenarios/features.c
@@ -1,6 +1,8 @@
+#include "raylib.h"
 #include "scenario-core.h"
 #include "raymath.h"
-#include "bnd-math.h"
+
+bnd_mesh_handle cone_mesh;
 
 void scenario_configure(program_config *config, bnd_config *physics_config) {
   config->window_title = "Features";
@@ -9,19 +11,13 @@ void scenario_configure(program_config *config, bnd_config *physics_config) {
 }
 
 void scenario_initialize(bnd_world *world) {
-
+  import_raylib_mesh(world, GenMeshCone(2, 3, 16), &cone_mesh);
 }
 
 void scenario_setup_scene(bnd_world *world) {
   bnd_add_box_static(world, (bnd_v3) { 10, 1, 10 });
 
-  bnd_body_shape shapes[] = {
-    (bnd_body_shape) { .type = BND_BOX, .offset = bnd_v3_zero(), .rotation = bnd_quat_identity(), .value = { .box = { .size = (bnd_v3) { 1.5, 1.5, 1.5 } } } },
-    (bnd_body_shape) { .type = BND_SPHERE, .offset = (bnd_v3) { 0, 1.5, 0 }, .rotation = bnd_quat_identity(), .value = { .sphere = { .radius = 0.75 } } },
-  };
-  float masses[] = { 2, 1 };
-
-  bnd_body_handle body = bnd_add_compound_body_dynamic(world, shapes, masses, 2).value;
+  bnd_body_handle body = bnd_add_mesh_dynamic(world, 5, cone_mesh).value;
 
   bnd_set_position(world, body, (bnd_v3) { 0, 7, 0 });
   bnd_set_rotation(world, body, QuaternionFromEuler(PI * 0.25, 0, 0));
