751bb6c1f21eb659bae3e42770d42886425a6a8b
Serhii Pikhota <serhii.pikhota@gmail.com>
2026-08-07 21:53:10 +0200

    Render EPA polytope

--- demos/scenarios/stack.c
+++ demos/scenarios/stack.c
@@ -1,5 +1,10 @@
+#include "raylib.h"
 #include "scenario-core.h"
+#include "bnd-core.h"
 #include "bnd-math.h"
+
+bnd_body_handle box_top;
+bnd_body_handle box_underneath;
 
 void scenario_configure(program_config *config, bnd_config *physics_config) {
   config->window_title = "Stack";
@@ -22,14 +27,47 @@ void scenario_setup_scene(bnd_world *world) {
     bnd_set_position(world, box, center);
     
     center = bnd_v3_add(center, (bnd_v3) { 0, box_size, 0 });
+
+    if (i == 4) {
+      box_top = box;
+    } else if (i == 3) {
+      box_underneath = box;
+    }
   }
 }
 void scenario_handle_input(bnd_world *world, Camera *camera) {
 }
 
-void scenario_simulate(bnd_world *world, float dt) { bnd_simulate(world, dt); }
+void scenario_simulate(bnd_world *world, float dt) {
+  if (world->age < 121) {
+    bnd_simulate(world, dt);
+  }
 
-void scenario_draw_scene(bnd_world *world) {}
+}
+
+void draw_face(bnd_v3 a, bnd_v3 b, bnd_v3 c, bnd_debug_epa_flags flags, void *user_data) {
+  Color color = flags & DEBUG_EPA_FACE_NEAREST ? GREEN : ORANGE;
+  DrawTriangle3D(a, b, c, color);
+
+  DrawLine3D(a, b, BLACK);
+  DrawLine3D(b, c, BLACK);
+  DrawLine3D(c, a, BLACK);
+}
+
+void draw_normal(bnd_v3 origin, bnd_v3 unit_normal, bnd_debug_epa_flags flags, void *user_data) {
+  draw_arrow(origin, bnd_v3_scale(unit_normal, 0.1), RED);
+}
+
+void draw_support(bnd_v3 point, void *user_data) {
+  
+}
+
+void scenario_draw_scene(bnd_world *world) {
+  if (world->age == 121) {
+    debug_epa_begin(world, box_underneath, box_top);
+    debug_epa_iteration(world, box_underneath, box_top, 2, (bnd_debug_draw_epa_callbacks) { draw_face, draw_normal, draw_support }, NULL);
+  }
+}
 
 void scenario_build_ui(bnd_world *world) {
 }
