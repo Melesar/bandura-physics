33f1e831b6e57e16bfc30f37026e524fcff2f518
Serhii Pikhota <serhii.pikhota@proton.me>
2026-09-21 13:00:06 +0200

    Add gap between boxes in stack scenario

--- demos/scenarios/stack.c
+++ demos/scenarios/stack.c
@@ -25,6 +25,8 @@ void scenario_configure(program_config *config, bnd_config *physics_config) {
   config->camera_position = (bnd_v3){0, 5, -10};
   config->camera_target = (bnd_v3){0, 2, 10};
 
+  physics_config->solver.warm_start = true;
+
   // config->draw_ground = false;
 }
 
@@ -35,6 +37,7 @@ void scenario_initialize(bnd_world *world) {
 void scenario_setup_scene(bnd_world *world) {
   const float floor_thikness = 0.5;
   const float box_size = 1;
+  const float gap = 0.2;
   const int stack_height = 5;
 
   bnd_add_box_static(world, (bnd_v3) { 30, floor_thikness, 30 });
@@ -44,7 +47,7 @@ void scenario_setup_scene(bnd_world *world) {
     bnd_body_handle box = bnd_add_box_dynamic(world, 3, (bnd_v3) { box_size, box_size, box_size }).value;
     bnd_set_position(world, box, center);
 
-    center = bnd_v3_add(center, (bnd_v3) { 0, box_size, 0 });
+    center = bnd_v3_add(center, (bnd_v3) { 0, box_size + gap, 0 });
 
     switch(i) {
       case 0: box_bottom = box;      break;
